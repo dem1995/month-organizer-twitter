fbe4d9c90a88c911ee1e17607009c4584c5557df
dem1995 <belmont95@live.com>
2020-12-17 12:29:33 -0600

    used string date instead of variable date, lol

--- filter_months.py
+++ filter_months.py
@@ -10,5 +10,5 @@ with open(args.tweet_file) as tweet_file:
 	for tweet_json in tweet_file:
 		tweet_dict = json.loads(tweet_json)
 		date = tweet_dict['created_at']
-		date = datetime.strptime("date", "%a %b %d %H:%M:%S %z %Y")
+		date = datetime.strptime(date, "%a %b %d %H:%M:%S %z %Y\n")
 		print(date)
