54aea8e85abbeca4addc13b73d279c0cf82386b1
dem1995 <belmont95@live.com>
2020-12-17 12:27:04 -0600

    Adding initial filter_months.py script (in-progress)

---
+++ filter_months.py
@@ -0,0 +1,14 @@
+import json
+import argparse
+from datetime import datetime
+
+parser = argparse.ArgumentParser("")
+parser.add_argument("tweet_file", help= "A jsonl file of tweets to parse")
+args = parser.parse_args()
+
+with open(args.tweet_file) as tweet_file:
+	for tweet_json in tweet_file:
+		tweet_dict = json.loads(tweet_json)
+		date = tweet_dict['created_at']
+		date = datetime.strptime("date", "%a %b %d %H:%M:%S %z %Y")
+		print(date)
